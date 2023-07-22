3e17ac8a9e74ca8fea45ef09a468ffc25610305c
Davy-og <ariembadavy@gmail.com>
2023-07-22 13:37:06 +0300

    Updated templates and views for CRUD functionality

--- Call_Of_Booty/urls.py
+++ Call_Of_Booty/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('insert', views.insert, name="insert"),
-    path('people/', views.people, name='people'),
-    path('contact_us/', views.contact, name='contact_us'),
+    path('insert/', views.insert, name="insert"),
+    path('people/', views.people, name="people"),
+    path('contact_us/', views.contact, name="contact_us"),
+    path('delete/<id>', views.delete, name='delete'),
+    path('update/<id>', views.update, name='update'),
 ]
--- Call_Of_Booty/views.py
+++ Call_Of_Booty/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import People
 from .forms import ContactForm
-from .models import Contact
 
 def home(request):
     return render(request, 'index.html')
+
+def get_next_id():
+    # Custom function to get the next available primary key value
+    last_record = People.objects.last()
+    if last_record is not None:
+        return last_record.id + 1
+    return 1  # If there are no records, start with ID 1
 
 def insert(request):
     if request.method == "POST":
@@ -13,10 +19,16 @@ def insert(request):
         school = request.POST.get('school')
         email = request.POST.get('email')
 
-        person = People(name=name, school=school, email=email)
+        # Get the next available primary key value
+        next_id = get_next_id()
+
+        # Manually assign the primary key and save the record
+        person = People(id=next_id, name=name, school=school, email=email)
         person.save()
 
-    return HttpResponse("Success")
+        return HttpResponse("Success")
+
+    return HttpResponse("No data received")
 
 def people(request):
     data = People.objects.all()
@@ -33,3 +45,13 @@ def contact(request):
 
     return render(request, 'contact_us.html', {"form": form})
 
+def delete(request, id):
+    dd = People.objects.get(id=id)
+    dd.delete()
+
+    return HttpResponse("Delete successful")
+
+def update(request, id):
+    l = People.objects.get(id=id)
+
+    return render(request, "edit.html", {"l": l})
